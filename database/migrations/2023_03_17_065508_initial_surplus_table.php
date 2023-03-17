f52c267d562e80856a9f406aceaf61ff937f0f62
ihsan maulana <ihsan.maulana@paper.id>
2023-03-17 14:49:26 +0700

    migration initial db

---
+++ database/migrations/2023_03_17_065508_initial_surplus_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->boolean('enable')->default(true);
+        });
+
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->boolean('enable')->default(true);
+        });
+
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('category_id');
+            $table->foreign('product_id')->references('id')->on('product');
+            $table->foreign('category_id')->references('id')->on('category');
+        });
+
+        Schema::create('image', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('file');
+            $table->boolean('enable')->default(true);
+        });
+
+        Schema::create('product_image', function (Blueprint $table) {
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('image_id');
+            $table->foreign('product_id')->references('id')->on('product');
+            $table->foreign('image_id')->references('id')->on('image');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category');
+        Schema::dropIfExists('product');
+        Schema::dropIfExists('category_product');
+        Schema::dropIfExists('image');
+        Schema::dropIfExists('product_image');
+    }
+};
